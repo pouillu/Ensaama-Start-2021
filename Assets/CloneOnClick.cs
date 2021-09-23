bd02bd75bc98e0e39cd1c9ae98eab2c9292b171f
pouillu <ghislainzell1@gmail.com>
2021-09-23 12:38:26 +0200

    iniy

--- Assets/CloneOnClick.cs
+++ Assets/CloneOnClick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CloneOneClick : MonoBehaviour
+public class CloneOnClick : MonoBehaviour
 {
     public int numberOfClones = 30;
 
